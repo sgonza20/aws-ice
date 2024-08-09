6fd62c23cc287b98ab59ddd35ba0800b5dca65a8
Sam Lozano <sgonza20@vols.utk.edu>
2024-08-08 22:01:15 -0400

    feat: delete terminated instances

--- amplify/functions/describe-instances/handler.ts
+++ amplify/functions/describe-instances/handler.ts
@@ -4,9 +4,11 @@ import {
   DescribeInstanceInformationCommandOutput,
   InstanceInformation,
 } from "@aws-sdk/client-ssm";
+import { generateClient } from "aws-amplify/data";
 import type { Schema } from "../../data/resource";
 
 const ssmClient = new SSMClient();
+const client = generateClient<Schema>();
 
 export const fetchInstances = async () => {
   try {
@@ -18,8 +20,7 @@ export const fetchInstances = async () => {
         NextToken: nextToken,
       });
 
-      const data: DescribeInstanceInformationCommandOutput =
-        await ssmClient.send(command);
+      const data: DescribeInstanceInformationCommandOutput = await ssmClient.send(command);
 
       if (data.InstanceInformationList) {
         allInstances = allInstances.concat(data.InstanceInformationList);
@@ -42,13 +43,31 @@ export const fetchInstances = async () => {
   }
 };
 
-export const handler: Schema["GetInstances"]["functionHandler"] = async (
-  event: any
-) => {
+export const handler: Schema["GetInstances"]["functionHandler"] = async (event: any) => {
   try {
-    const instances = await fetchInstances();
-    console.log("Fetched EC2 Instances:", instances);
-    return instances;
+    const { data: existingInstances } = await client.models.Instance.list();
+    const existingInstanceIds = existingInstances.map((instance) => instance.InstanceId);
+
+    const fetchedInstances = await fetchInstances();
+    const fetchedInstanceIds = fetchedInstances.map((instance) => instance.InstanceId);
+
+    console.log("Fetched EC2 Instances:", fetchedInstances);
+
+    const instancesToDelete = existingInstanceIds.filter((instanceId) => !fetchedInstanceIds.includes(instanceId));
+
+    for (const instanceId of instancesToDelete) {
+      const instanceToDelete = { InstanceId: instanceId };
+      const { data: deletedInstance, errors } = await client.models.Instance.delete(instanceToDelete);
+
+      if (errors) {
+        console.error(`Error deleting instance ${instanceId}:`, errors);
+      } else {
+        console.log(`Deleted instance ${instanceId}:`, deletedInstance);
+      }
+    }
+
+    // Step 4: Return the fetched instances
+    return fetchedInstances;
   } catch (error) {
     console.error("Error handling request:", error);
     return undefined;
