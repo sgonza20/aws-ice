0e6846a48faab7fce1587275d59b2a3ce28a9b6a
Sam Lozano <sgonza20@vols.utk.edu>
2024-08-11 21:53:14 -0400

    chore: add command id

--- amplify/functions/invoke-ssm/handler.ts
+++ amplify/functions/invoke-ssm/handler.ts
@@ -3,11 +3,9 @@ import {
   SendCommandCommand,
   SendCommandCommandOutput,
 } from "@aws-sdk/client-ssm";
-import { SFNClient, StartExecutionCommand } from "@aws-sdk/client-sfn";
 import type { Schema } from "../../data/resource";
 
 const ssmClient = new SSMClient();
-const StepFunctionClient = new SFNClient();
 
 export const handler: Schema["InvokeSSM"]["functionHandler"] = async (
   event: any
@@ -42,56 +40,34 @@ export const handler: Schema["InvokeSSM"]["functionHandler"] = async (
   console.log("Invoking SSM document with arguments:", event.arguments);
   try {
     const command = new SendCommandCommand({
-      InstanceIds: [InstanceId],
+      InstanceIds: [event.arguments.InstanceId],
       DocumentName: DocumentName,
       Parameters: {
-        OS: [OS],
-        Benchmark: [Benchmark],
+        OS: [event.arguments.OS],
+        Benchmark: [event.arguments.Benchmark],
       },
     });
 
     const data: SendCommandCommandOutput = await ssmClient.send(command);
     const commandId = data.Command?.CommandId;
 
+
     console.log("Command ID", commandId);
     if (!commandId) {
-      return {
-        statusCode: 500,
-        body: "Failed to retrieve CommandId",
-      };
+        return {
+            statusCode: 500,
+            body: "Failed to retrieve CommandId",
+        };
     }
-
-    // Start Step Functions Execution
-    const stepFunctionsInput = {
-      commandId,
-      instanceId: InstanceId,
+    return {
+      statusCode: 200,
+      body: commandId,
     };
-
-    try {
-      const stepFunctionsCommand = new StartExecutionCommand({
-        stateMachineArn: process.env.STATE_MACHINE_ARN, // Ensure this is set as an environment variable
-        input: JSON.stringify(stepFunctionsInput),
-      });
-
-      const stepFunctionsResponse = await StepFunctionClient.send(stepFunctionsCommand);
-      console.log("State Machine execution started:", stepFunctionsResponse.executionArn);
-
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ commandId, executionArn: stepFunctionsResponse.executionArn }),
-      };
-    } catch (stepFunctionError) {
-      console.error("Error starting Step Function:", stepFunctionError);
-      return {
-        statusCode: 500,
-        body: "Failed to start Step Function",
-      };
-    }
   } catch (error) {
     console.error("Error invoking SSM document:", error);
     return {
       statusCode: 500,
       body: "Failed to invoke SSM document",
-    };
+      }
   }
 };
